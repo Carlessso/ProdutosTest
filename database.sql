e838de93c15996f767fcea1882e70fec3a9c1f94
Carlessso <matheuscarlesso1998@gmail.com>
2022-02-16 22:21:44 -0300

    first commit

---
+++ database.sql
@@ -0,0 +1,21 @@
+create database produtos;
+
+CREATE TABLE produto(
+    id serial not null PRIMARY KEY,
+    nome text not null,
+    preco numeric(10,3) not null,
+    quantidade numeric(10,3) not null
+);
+
+CREATE TABLE categoria(
+    id serial not null PRIMARY KEY,
+    nome text not null
+);
+
+CREATE TABLE produto_categoria(
+    id serial not null PRIMARY KEY,
+    produto_id integer not null,
+    categoria_id integer not null,
+    CONSTRAINT fk_produto FOREIGN KEY (produto_id) REFERENCES produto(id),
+    CONSTRAINT fk_categoria FOREIGN KEY (categoria_id) REFERENCES categoria(id) 
+);
